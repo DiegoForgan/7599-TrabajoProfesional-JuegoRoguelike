79af9b65ca00ef31fe6a0369af00fef7d8ee3cae
DiegoForgan <dmforgan89@gmail.com>
2021-09-26 04:31:10 -0300

    added comment on how attackrate stat works

--- First Try Roguelike/Assets/Scripts/GameStats/PlayerStats/EntityData.cs
+++ First Try Roguelike/Assets/Scripts/GameStats/PlayerStats/EntityData.cs
@@ -6,6 +6,8 @@ public abstract class EntityData : ScriptableObject
 {
     public int health;
     public float movementSpeed;
+    //The bigger the number, te faster it can attack
+    //Use numbers smaller than 1 to make time beetween attacks slower!
     public float attackRate;
     public Sprite sprite;
 }
