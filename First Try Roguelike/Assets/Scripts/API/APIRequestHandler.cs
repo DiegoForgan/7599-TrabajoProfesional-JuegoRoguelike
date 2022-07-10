12e45ee61d85fd95b66b6f8c0181e606b6b946a8
DiegoForgan <dmforgan89@gmail.com>
2022-07-10 01:24:47 -0300

    Registers New Users from UI

--- First Try Roguelike/Assets/Scripts/API/APIRequestHandler.cs
+++ First Try Roguelike/Assets/Scripts/API/APIRequestHandler.cs
@@ -10,7 +10,7 @@ public class APIRequestHandler : MonoBehaviour
 {
     private const string QA_URL = "http://fiuba-qa-7599-cs-app-server.herokuapp.com/api/v1/users";
     private const string DEV_URL = ""; // TO DEFINE
-    private string testString = "{ \"username\": \"juan05146\", \"password\": \"123456\", \"first_name\": \"Damián\", \"last_name\": \"Marquesín Fernandez\", \"contact\": { \"email\": \"juanmg0511@gmail.com\", \"phone\": \"5555 5555\" }}";
+    //private string testString = "{ \"username\": \"juan05146\", \"password\": \"123456\", \"first_name\": \"Damián\", \"last_name\": \"Marquesín Fernandez\", \"contact\": { \"email\": \"juanmg0511@gmail.com\", \"phone\": \"5555 5555\" }}";
 
     public void CheckUsernameAlreadyTaken(){
         StartCoroutine(CheckUsernameRequest());
@@ -57,22 +57,24 @@ public class APIRequestHandler : MonoBehaviour
         else Debug.Log("Request resulted in unknown error");
     }
 
-    /*private IEnumerator RegisterNewUser(string username, string password, string first_name, string last_name,
-     string email, string phone, bool isUrl, string data, bool login_service){
-        Debug.Log("Register New User with parameters...");
-        
-    }*/
-
-    private string CreateNewUserDataJSON(string newuser){
-        return "{ \"username\": \""+newuser+"\", \"password\": \"123456\", \"first_name\": \"Damián\", \"last_name\": \"Marquesín Fernandez\", \"contact\": { \"email\": \"juanmg0511@gmail.com\", \"phone\": \"5555 5555\" }}";
+    private string GetJsonStringRegisterData(){
+        // Getting all register data from input fields (maybe should optimize this!)
+        string newUsername = GameObject.Find("UserNameInputField").GetComponent<TMP_InputField>().text;
+        string newPassword = GameObject.Find("PasswordInputField").GetComponent<TMP_InputField>().text;
+        string newFirstName = GameObject.Find("FirstNameInputField").GetComponent<TMP_InputField>().text;
+        string newLastName = GameObject.Find("LastNameInputField").GetComponent<TMP_InputField>().text;
+        string newEmail = GameObject.Find("EmailInputField").GetComponent<TMP_InputField>().text;
+        string newPhone = GameObject.Find("PhoneInputField").GetComponent<TMP_InputField>().text;
+        //TO DO: ADD missing info to the json -> Avatar and login service
+        return "{ \"username\": \""+newUsername+"\", \"password\": \""+newPassword+"\", \"first_name\": \""+newFirstName+"\", \"last_name\": \""+newLastName+"\", \"contact\": { \"email\": \""+newEmail+"\", \"phone\": \""+newPhone+"\" }}";
     }
 
     private IEnumerator RegisterNewUserRequest(){
-        Debug.Log("Registering new user without parameters to the game...");
-        Debug.Log(CreateNewUserDataJSON("diegote"));
-        // The unitywebRequest library its pretty tricky, for POST method you should start with PUT and then change it on the next lines
+        Debug.Log("Registering new user to the game...");
+        string registerDataJson = GetJsonStringRegisterData();
+        // The UnityWebRequest library its pretty tricky, for POST method you should start with PUT and then change it on the next lines
         // Implementation based on the tutorial found at https://manuelotheo.com/uploading-raw-json-data-through-unitywebrequest/
-        UnityWebRequest request = UnityWebRequest.Put(QA_URL, testString);
+        UnityWebRequest request = UnityWebRequest.Put(QA_URL, registerDataJson);
         request.method = UnityWebRequest.kHttpVerbPOST;
         
         request.SetRequestHeader("Content-Type", "application/json");
@@ -80,8 +82,17 @@ public class APIRequestHandler : MonoBehaviour
         
         yield return request.SendWebRequest();
         
-        Debug.Log("Status Code: " + request.responseCode);
-        Debug.Log("Response: " + request.downloadHandler.text);
+        //Response handling
+        if(request.result == UnityWebRequest.Result.ProtocolError){
+            Debug.Log("Missing data to fullfill the request or User already exists");
+        }
+        else if(request.result == UnityWebRequest.Result.Success){
+            Debug.Log("Success");
+        }
+        else Debug.Log(request.result);
+        //Debug.Log("Status Code: " + request.responseCode);
+        //Debug.Log("Response: " + request.downloadHandler.text);
+        //Debug.Log(request.result);
     }
 
    
