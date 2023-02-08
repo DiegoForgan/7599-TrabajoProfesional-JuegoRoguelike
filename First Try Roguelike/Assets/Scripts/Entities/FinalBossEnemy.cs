c6158a3c31d51acae59a4ceef271132ee0f43ff0
DiegoForgan <dmforgan89@gmail.com>
2023-02-08 03:52:03 -0300

    getting closer

--- First Try Roguelike/Assets/Scripts/Entities/FinalBossEnemy.cs
+++ First Try Roguelike/Assets/Scripts/Entities/FinalBossEnemy.cs
@@ -1,46 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinalBossEnemy : Enemy
+public class FinalBossEnemy : MonoBehaviour
 {
-    /*[SerializeField] private EnemyLongRangeMovement longRangeMovement;
+    [SerializeField] private MeleeEnemy meleeEnemy;
     [SerializeField] private EnemyMovement enemyMovement;
-    private EnemyMovement currentMovementType;
-    
-    private float meleeDistance;
-    private bossPhase currentPhase;
-    private enum bossPhase {SpellCaster, MeleeAtacker, Mix };
+    [SerializeField] private SpellCasterEnemy spellCasterEnemy;
+    [SerializeField] private EnemyLongRangeMovement enemyLongRangeMovement;
+    [SerializeField] private Enemy enemy;
 
-    private void Awake()
+    private enum FinalBossState { MeleeAttacker, SpellCasterAttacker}
+
+    private FinalBossState currentBossState;
+    private const float FINAL_BOSS_STATE_THRESHOLD = 0.5f;
+
+    private void Start()
     {
-        currentPhase = bossPhase.MeleeAtacker;
-        longRangeMovement.enabled = false;
-        
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        meleeWeapon = GetComponent<MeleeWeapon>();
-        currentMovementType = enemyMovement;
-        _attackPoint = transform.Find("AttackPoint");
-        animator = GetComponent<CharactersAnimator>();
+        currentBossState = FinalBossState.SpellCasterAttacker;
+    }
 
-        if (!playerTransform) Debug.LogError("No player Transform aquired!");
-        if (!_attackPoint) Debug.LogError("No attack point found!");
-    }*/
-
-    /*private void Start()
+    private void Update()
     {
-        InitHealth();
-        
-        meleeWeapon.SetWeaponData(enemyData.availableMeleeWeapon);
-        //distance has to be tweaked in order to respond to the attacking point
-        meleeDistance = Vector2.Distance(transform.position, _attackPoint.position) + enemyData.availableMeleeWeapon.range;
-        
-        
-        //Creates a copy of the list to prevent changes on the Scriptable Object
-        availableSpells = new List<SpellData>(enemyData.availableSpell);
-        distance = enemyData.attackDistance;
-        
-        InitMovementStats(distance);
-    }*/
+        SetScriptsBasedOnBossState();
+        UpdateBossState();
+    }
+
+    private void UpdateBossState()
+    {
+        Debug.Log("Nilbud Remaining Health Percentage: " + GetEnemyRemainingHealthPercentage() * 100);
+        if (GetEnemyRemainingHealthPercentage() < FINAL_BOSS_STATE_THRESHOLD) setBossState(FinalBossState.MeleeAttacker);
+    }
+
+    private float GetEnemyRemainingHealthPercentage()
+    {
+        return enemy.GetHealth() / enemy.GetMaxHealth();
+    }
+
+    private void SetScriptsBasedOnBossState()
+    {
+        if (currentBossState == FinalBossState.MeleeAttacker) EnableMeleeScripts();
+        else if (currentBossState == FinalBossState.SpellCasterAttacker) EnableSpellCastingScripts();
+    }
+
+    private void setBossState(FinalBossState newState)
+    {
+        Debug.Log("Changing Nilbud Attack State");
+        currentBossState = newState;
+    }
+
+    private void EnableSpellCastingScripts()
+    {
+        meleeEnemy.enabled = false;
+        enemyMovement.enabled = false;
+        spellCasterEnemy.enabled = true;
+        enemyLongRangeMovement.enabled = true;
+    }
+
+    private void EnableMeleeScripts()
+    {
+        meleeEnemy.enabled = true;
+        enemyMovement.enabled = true;
+        spellCasterEnemy.enabled = false;
+        enemyLongRangeMovement.enabled = false;
+    }
 }
 
